f74769e18de37faec9a5c914d3237ffb889cd91a
Mathias Oberson <mathias.oberson@heig-vd.ch>
2018-07-02 17:32:13 +0200

    Add the code

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  KnexNonceStore: require('./lib/knex-nonce-store.class'),
+  isFreshTimestamp: require('./lib/is-fresh-timestamp'),
+  isTimestamp: require('./lib/is-timestamp')
+};
